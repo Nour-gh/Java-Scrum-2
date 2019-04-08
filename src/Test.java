895796877520c18cfa3a9afbd0c88dd9726b5189
Project49Music <48435515+Project49Music@users.noreply.github.com>
2019-04-08 11:03:23 +0200

    1

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.print("hou");
+		System.out.print("hou,knf,sdn");
 
 		System.out.print("hzsfwettaei");
 		System.out.print("hoihouhur");
