26ba9003e3bc4822a03bdaaf3ea70c9e65452091
Noelleyasmijn <47447095+Noelleyasmijn@users.noreply.github.com>
2019-04-08 10:46:00 +0200

    test

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,12 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		System.out.print("hou");
+
+		System.out.print("hi");
+		System.out.print("hoihouhur");
+
 	}
 
 }
