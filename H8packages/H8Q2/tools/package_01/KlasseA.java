5eae7a8db84d151b2c66312121c4c2f5de6fd1a3
wybrenmulder <wkh1304@gmail.com>
2019-04-11 18:30:06 +0200

    h8 packages

---
+++ H8packages/H8Q2/tools/package_01/KlasseA.java
@@ -0,0 +1,14 @@
+package H8Q2.tools.package_01;
+import H8Q2.tools.package_02.KlasseB;
+
+public class KlasseA {
+	public static void main(String[] args) {
+		
+		KlasseB kb = new KlasseB();
+		System.out.print(kb.mijnInt); /* 0 */
+		System.out.print(kb.w); /* 1 */
+		// System.out.print(kb.x); /* 2 */
+		// System.out.print(kb.y); /* 3 */
+		// System.out.print(kb.z); /* 4 */
+	}
+}
