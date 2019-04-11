fec172ed0084c535bc2dfc99120c6ad5b255f1bf
Project49Music <48435515+Project49Music@users.noreply.github.com>
2019-04-11 18:52:39 +0200

    Testfile updated

--- src/Test.java
+++ src/Test.java
@@ -2,28 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		// TODO Auto-generated method stub
-=======
-		// TODO Auto-generated method stub
 
-		System.out.print("hou,knf,sdn");
-
-
-		System.out.print("hi42");
-
-		System.out.print("hzsfwettaei");
-
-		System.out.print("hoihouhur");
-
-		
-
-		System.out.print("hoihouididi");
-
-
-		System.out.print("hoihouhur");
->>>>>>> branch 'master' of https://github.com/Project49Music/Java-Scrum-1.git
-
+		System.out.print("Dit is een test");
 	}
-
 }
