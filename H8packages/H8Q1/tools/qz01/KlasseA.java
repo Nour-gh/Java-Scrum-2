bfa89a651bbb0cee5c00447b3741abb808307315
wybrenmulder <wkh1304@gmail.com>
2019-04-11 18:32:26 +0200

    h8

---
+++ H8packages/H8Q1/tools/qz01/KlasseA.java
@@ -0,0 +1,12 @@
+package H8Q1.tools.qz01;
+
+public class KlasseA {
+	public static void main(String[] args) {
+		KlasseB kb = new KlasseB();
+		// System.out.print(kb.mijnChar); /* 0 */
+		System.out.print(kb.w); /* 1 */
+		System.out.print(kb.x); /* 2 */
+		System.out.print(kb.y); /* 3 */
+		// System.out.print(kb.z); /* 4 */
+	}
+}
