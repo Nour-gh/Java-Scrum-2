7f0f3b07a940ce43a02f9bb61d66a50bb4770ed4
Project49Music <48435515+Project49Music@users.noreply.github.com>
2019-04-11 20:44:17 +0200

    Heel hoofdstuk 9 overgetypt van Nours document Arraylist-verslag.docx, Nour heeft heel H9 gemaakt.

---
+++ src/hoofdstuk9/src/Quiz8/Student.java
@@ -0,0 +1,13 @@
+package hoofdstuk9.src.Quiz8;
+
+public class Student {
+    String naam;
+    int leeftijd;
+    String email;
+
+    public Student(String naam, int leeftijd, String email) {
+        this.naam = naam;
+        this.leeftijd = leeftijd;
+        this.email = email;
+    }
+}
